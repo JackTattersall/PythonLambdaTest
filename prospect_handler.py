0e27476f9cf9c2a415869341478a3392805ed7d6
JackTattersall <jack.tattersall@opencastsoftware.com>
2018-03-19 10:32:09 +0000

    adds jwt

--- prospect_handler.py
+++ prospect_handler.py
@@ -1,12 +1,24 @@
+import jwt
+
 
 def handler(event, context):
     data = event['body']
+    decoded_jwt = None
+    try:
+        decoded_jwt = jwt.decode(event['headers']['Authorization'], 'paigeleah1')
+    except Exception as e:
+        pass
+
     return {
         'statusCode': 200,
-        'body': data,
+        'body': {
+            'person': data,
+            'jwt': decoded_jwt or ""
+        },
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': 'www.dave.com',
-            'Access-Control-Allow-Methods': 'OPTION,POST'
+            'Access-Control-Allow-Methods': 'OPTION,POST',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         }
     }
--- tests/test_risk_handler.py
+++ tests/test_risk_handler.py
@@ -4,14 +4,13 @@ import prospect_handler
 
 
 class TestHandlerCase(unittest.TestCase):
-
     def test_hello_world_response(self):
         print("testing response.")
         result = risk_handler.handler({"body": "test body"}, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('test body', result['body'])
+        self.assertIn('test body', result['body']['person'])
 
     def test_hello_response(self):
         print("testing response.")
@@ -19,7 +18,7 @@ class TestHandlerCase(unittest.TestCase):
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('test body', result['body'])
+        self.assertIn('test body', result['body']['person'])
 
 
 if __name__ == '__main__':
